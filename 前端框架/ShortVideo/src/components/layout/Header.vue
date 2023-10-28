a07926e7884051b9d050cf75024ce7c67e6d13d2
Xguihur <329179227@qq.com>
2023-10-28 16:55:24 +0800

    引入 less、antdV

--- 前端框架/ShortVideo/src/components/layout/Header.vue
+++ 前端框架/ShortVideo/src/components/layout/Header.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>顶部组件</div>
+  <div>
+    <div>顶部组件</div>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="primary">Dashed Button</a-button>
+  </div>
 </template>
 <script setup></script>
 <style scoped lang="less"></style>
